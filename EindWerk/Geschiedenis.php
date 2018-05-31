74a787eb1018817eb7dd5d93f3af69477c91c22e
rolceroyce70 <rolceroyce70@gmail.com>
2018-05-31 07:09:26 -0400

    bezig met ges

--- EindWerk/Geschiedenis.php
+++ EindWerk/Geschiedenis.php
@@ -4,15 +4,34 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" type="text/css" href="CSS/Geschiedenis.css">
   <link rel="stylesheet" type="text/css" href="CSS/Style.css">
 </head>
 <body>
-   <div id="gridContainer">
-      <div id="menu">
-      </div>
+  <div id="geschiedenis">
+    <div id="menu">
+      <a href="Home.php">Home</a>
+      <a href="#">Geschiedenis</a>
+      <a href="VerschillendeSporten.php">Verschillende Sporten</a>
+      <a href="BekendeSporters.php">Bekende Sporters</a>
+      <a href="Plaatsen.php">Plaatsen</a>
+      <a href="Contact.php">Contact</a>
+    </div>
       
-      <div id="home">
-      </div>
+    <div id="titelG">
+      <h3>Geschiedenis</h3>
+    </div>
+    <div id="tekstG">
+      <p>
+        hahahahahaha
+      </p>
+    </div>
+    <div id="slideshowG">
+    
+    </div>
+    <div id="footer">
+      <p>hahaha</p>
+    </div>
   </div>
 </body>
 </html>
